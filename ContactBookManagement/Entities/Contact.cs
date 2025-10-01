2973c8e8c5d23895e108183b05f84bcd1d5a285f
Zeena Yalda <zyalda@hotmail.com>
2025-10-01 20:57:54 +0200

    Create entities directory and contact class.

---
+++ ContactBookManagement/Entities/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactBookManagement.Entities
+{
+    public class Contact
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public string City { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "EmailIsRequired")]
+        public string EmailAddress { get; set; }
+    }
+}
