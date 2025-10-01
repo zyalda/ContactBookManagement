d4d84d8910d6b6451958074a39232d99aa358fde
Zeena Yalda <zyalda@hotmail.com>
2025-10-01 20:49:36 +0200

    Create repository class.

---
+++ ContactBookManagement/Repositories/ContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ContactBookManagement.Repositories
+{
+    public class ContactRepository
+    {
+        //CRUD principle....
+        public void AddContact(Contact contact)
+        {
+            //if (ValidationMethod(contact))
+            //    //Update text file
+            //else
+            Console.WriteLine("Add contact failed, try again.");
+        }
+    }
+}
