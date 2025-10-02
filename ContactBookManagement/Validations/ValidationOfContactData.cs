755520adc680da92e1fde296a42ed497259aba68
Zeena Yalda <zyalda@hotmail.com>
2025-10-02 09:57:44 +0200

    Create GUI validation class and add validating in to textboxes.

---
+++ ContactBookManagement/Validations/ValidationOfContactData.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ContactBookManagement.Validations
+{
+    public class ValidationOfContactData
+    {
+        private readonly Regex _reg;
+        public ValidationOfContactData()
+        {
+            _reg = new Regex("\\A[^\\W\\d_]+\\z");
+        }
+        public bool ValidatingOfStringData(string data)
+        {
+            if (!_reg.Match(data).Success)
+                return false;
+            return true;
+        }
+    }
+}
