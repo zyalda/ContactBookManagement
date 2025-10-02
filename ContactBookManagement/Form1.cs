20aac3d9a8a839f7223cd93eb68d811e684bfb79
Zeena Yalda <zyalda@hotmail.com>
2025-10-02 09:59:56 +0200

    Edit the form file.

--- ContactBookManagement/Form1.cs
+++ ContactBookManagement/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ContactBookManagement.Entities;
+using ContactBookManagement.Validations;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -6,6 +8,7 @@ namespace ContactBookManagement
 {
     public partial class ContactsForm : Form
     {
+        ValidationOfContactData validationOfContactData = new ValidationOfContactData();
         public ContactsForm()
         {
             InitializeComponent();
@@ -20,6 +23,42 @@ namespace ContactBookManagement
 
         private void ContactsForm_Load(object sender, EventArgs e)
         {
+        }
+
+        private void textFirstName_TextChanged(object sender, EventArgs e)
+        {
+            Contact contact = new Contact();
+            contact.FirstName = textFirstName.Text;
+            Console.WriteLine(contact.FirstName);
+        }
+
+        private void textFirstName_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!validationOfContactData.ValidatingOfStringData(textFirstName.Text))
+            {
+                MessageBox.Show($"{textFirstName.Text} is not valid.");
+                textFirstName.Text = string.Empty;
+                e.Cancel = true;
+            }
+        }
+        private void textLastName_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!validationOfContactData.ValidatingOfStringData(textLastName.Text))
+            {
+                MessageBox.Show($"{textLastName.Text} is not valid.");
+                textLastName.Text = string.Empty;
+                e.Cancel = true;
+            }
+        }
+
+        private void textCity_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!validationOfContactData.ValidatingOfStringData(textCity.Text))
+            {
+                MessageBox.Show($"{textCity.Text} is not valid.");
+                textCity.Text = string.Empty;
+                e.Cancel = true;
+            }
         }
     }
 }
